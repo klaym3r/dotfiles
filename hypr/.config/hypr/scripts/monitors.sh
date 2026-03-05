d00dfe8d5cf18bce2024b38ebad97b5c4dd97bdc
klaym3r <klaym3r@gmail.com>
2026-03-05 16:28:35 +0300

    latest

--- hypr/.config/hypr/scripts/monitors.sh
+++ hypr/.config/hypr/scripts/monitors.sh
@@ -3,13 +3,16 @@
 if hyprctl monitors | grep -q "HDMI-A-1"; then
   hyprctl reload
   hyprctl keyword monitor "HDMI-A-1, 1920x1080@120, 0x0, 1"
-  # hyprctl keyword monitor "eDP-1, disable"
-  hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x1080, 1.25"
-  hyprctl keyword workspace "1, monitor:HDMI-A-1"
-  hyprctl keyword workspace "2, monitor:HDMI-A-1"
-  hyprctl keyword workspace "3, monitor:HDMI-A-1"
-  hyprctl keyword workspace "4, monitor:HDMI-A-1"
-  hyprctl keyword workspace "5, monitor:HDMI-A-1"
+  
+  hyprctl keyword monitor "eDP-1, disable"
+  # hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x1080, 1.25"
+  # hyprctl keyword monitor "eDP-1, 1920x1080@60, auto, 1.25, mirror, HDMI-A-1"
+ 
+  # hyprctl keyword workspace "1, monitor:HDMI-A-1"
+  # hyprctl keyword workspace "2, monitor:HDMI-A-1"
+  # hyprctl keyword workspace "3, monitor:HDMI-A-1"
+  # hyprctl keyword workspace "4, monitor:HDMI-A-1"
+  # hyprctl keyword workspace "5, monitor:HDMI-A-1"
 else
   hyprctl reload
   hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x0, 1.25"
